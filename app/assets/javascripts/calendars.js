a778459312b7f94f269ef1fdf5c2d01688d01249
Jeri <drvonnjerryxlii@gmail.com>
2015-10-05 14:05:22 -0700

    wip adding fullcalendar.io

--- app/assets/javascripts/calendars.js
+++ app/assets/javascripts/calendars.js
@@ -1,2 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  // page is now ready, initialize the calendar...
+  // console.log("page is ready. initializing the calendar...");
+
+  $('#calendar').fullCalendar({
+    // put your options and callbacks here
+    header: { center: 'month,agendaWeek,agendaDay' }, // buttons for switching between views
+
+    views: {
+        month: { // name of view
+            titleFormat: 'YYYY, MM, DD'
+            // other view-specific options here
+        }
+    },
+
+    dayClick: function() {
+      alert('a day has been clicked!');
+    },
+
+    height: "auto"
+
+  })
+});

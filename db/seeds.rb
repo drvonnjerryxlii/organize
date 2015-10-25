df60bfc00d0a2a6355429b2d0471363d488ea1c7
Jeri <drvonnjerryxlii@gmail.com>
2015-10-25 11:09:24 -0700

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,40 @@ users.each do |user|
   )
 end
 
+puts "seeded special users"
+
+# plus we need a bunch of generic students
+emails = ["generic", "default", "example"]
+
+first_names = [
+  "Adanech", "Amabel", "Anouk", "Caia", "Carmelita", "Cerys", "Eshe",
+  "Fan", "Fathiyya", "Finn", "Gzifa", "Jia", "Jifunza", "Lin", "Nasim",
+  "Onaedo", "Ryoko", "Seul-gi"
+]
+
+last_names = [
+  "Arial", "Comic-Sans", "Courier", "Futura", "Garamound", "Georgia",
+  "Helvetia", "Impact", "Lucida", "Palatino", "Serif", "Times", "Trebuchet",
+  "Verdana", "Wingdings"
+]
+
+first_generic_student = User.last.id + 1
+
+(1...100).to_a.each do |n|
+  User.create(
+    email: "student#{ n }@shark.com",
+    github: "#{ emails.sample }#{ n }",
+    name: "#{ first_names.sample } #{ last_names.sample }",
+    type: "Student",
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+last_generic_student = User.last.id
+
+puts "seeded generic student users"
+
 broadcasts = CSV.read(file_base + "broadcasts.csv", { headers: true })
 broadcasts.each do |broadcast|
   Broadcast.create(
@@ -22,13 +56,17 @@ broadcasts.each do |broadcast|
   )
 end
 
+puts "seeded broadcasts"
+
 calendars = CSV.read(file_base + "cohorts.csv", { headers: true })
 calendars.each do |calendar|
-  c = Calendar.create(
+  Calendar.create(
     name: calendar["name"],
     google_calendar_id: calendar["google_calendar_id"]
   )
 end
+
+puts "seeded calendars"
 
 calendars_users = CSV.read(file_base + "cohorts_users.csv", { headers: true })
 calendars_users.each do |calendar_user|
@@ -45,6 +83,8 @@ notes.each do |note|
   )
 end
 
+puts "seeded notes"
+
 guest_lectures = CSV.read(file_base + "guest_lectures.csv", { headers: true })
 guest_lectures.each do |guest_lecture|
   GuestLecture.create(
@@ -54,17 +94,23 @@ guest_lectures.each do |guest_lecture|
   )
 end
 
+puts "seeded guest lectures"
+
 events = CSV.read(file_base + "events.csv", { headers: true })
 events.each do |event|
-  c = Calendar.find((1..5).to_a.sample)
-  e = Event.create(
+  Event.create(
     title: event["title"],
     start_time: event["start_time"],
     end_time: event["end_time"],
+    ta: event["ta"],
+    gl: event["gl"],
     guest_lecture_id: event["guest_lecture_id"],
-    calendar_id: c.id
+    user_id: event["user_id"],
+    calendar_id: event["calendar_id"]
   )
 end
+
+puts "seeded events"
 
 # events_users = CSV.read(file_base + "events_users.csv", { headers: true })
 # events_users.each do |event_user|
@@ -79,9 +125,13 @@ categories.each do |category|
     name: category["name"],
     hidden: category["hidden"],
     admin_only: category["admin_only"],
+    topic_related: category["topic_related"],
+    user_related: category["user_related"],
     approved: category["approved"]
   )
 end
+
+puts "seeded categories"
 
 category_joins = CSV.read(file_base + "category_joins.csv", { headers: true })
 category_joins.each do |category_join|
@@ -91,3 +141,13 @@ category_joins.each do |category_join|
     categorizable_type: category_join["categorizable_type"]
   )
 end
+
+cohorts = Category.where("name LIKE ?", "%cohort%")
+
+while (first_generic_student == last_generic_student) do
+  student = User.find(first_generic_student)
+  student.categories << cohorts.sample
+  first_generic_student += 1
+end
+
+puts "seeded categorizables"

6491fe2cf43a2e674adabc518fbc72381547b043
Jeri <drvonnjerryxlii@gmail.com>
2015-10-26 15:49:15 -0700

    stylin

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.chronological
   end
 
   def show; end

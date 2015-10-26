e7a66df316e15cbd4f82e8fefc1b743e6efde16b
Jeri <drvonnjerryxlii@gmail.com>
2015-10-26 15:17:45 -0700

    tweaks to battery stylins

--- app/assets/javascripts/calendars.js
+++ app/assets/javascripts/calendars.js
@@ -8,15 +8,18 @@ $(document).ready(function() {
     var day = visibleDays[i];
     var taCount = $(day).children().length;
 
-    // for any date after today, if < 4 TAs put an empty battery icon in
-    if (taCount < 1) {
-      $(day).addClass('vacancy');
-      var icon = $('<div class="icon"><i class="fa fa-battery-quarter"></i></div>');
-      $(day).append(icon);
-    } else {
-      $(day).addClass('full');
-      var icon = $('<div class="icon"><i class="fa fa-battery-full"></i></div>');
-      $(day).append(icon);
+    // for any weekday after today, if < 4 TAs put an empty battery icon in
+    // we don't care about weekends
+    if (!$(day).hasClass('wday-0') && !$(day).hasClass('wday-6')) {
+      if (taCount < 1) {
+        $(day).addClass('vacancy');
+        var icon = $('<div class="icon"><i class="fa fa-battery-quarter"></i></div>');
+        $(day).append(icon);
+      } else {
+        $(day).addClass('full');
+        var icon = $('<div class="icon"><i class="fa fa-battery-full"></i></div>');
+        $(day).append(icon);
+      }
     }
   }
 })

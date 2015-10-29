86d8ae9d0dfaeadc2a858c24f968f93e1ed9add2
Jeri <drvonnjerryxlii@gmail.com>
2015-10-29 14:02:04 -0700

    added datetimepickers to request shift form

--- app/assets/javascripts/calendars.js
+++ app/assets/javascripts/calendars.js
@@ -1,7 +1,34 @@
-// when document ready, check for event divs inside each date
-// for any weekday after today, if less than x TAs put an empty battery icon in
-// otherwise, put in a full battery icon
 $(document).ready(function() {
+  addIconsToCalendar();
+
+  // ----- setup for bootstrap datetime pickers --------------------------------
+  // 1. moment.js formats info: http://momentjs.com/docs/#/displaying/format/
+  //    datetimepicker will use the format to adjust what pickers are displayed.
+  // 2. weekends are disabled by default:
+  //    - daysOfWeekDisabled: [0, 6]
+  // 3. hours outside 1-8pm are disabled by default:
+  //    - enabledHours: [13, 14, 15, 16, 17, 18, 19, 20]
+  // NOTE: locales are not yet supported by these form elements. ;_;
+  $('.datetimepicker').datetimepicker({ daysOfWeekDisabled: [0, 6] });
+  $('.datepicker').datetimepicker({ format: 'll', daysOfWeekDisabled: [0, 6] });
+  $('.timepicker-start').datetimepicker({
+    format: 'LT',
+    enabledHours: [13, 14, 15, 16, 17],
+  });
+
+  $('.timepicker-end').datetimepicker({
+    format: 'LT',
+    enabledHours: [16, 17, 18, 19, 20],
+  });
+})
+
+// 1. when document is ready, check for event divs inside each date.
+// 2. for any weekday:
+//    - if less than x TAs*, display an empty battery icon.
+//      NOTE: x TAs is currently hard-coded to 3. see second line in for loop.
+//    - otherwise, display a full battery icon.
+//    - if pending events on that day, display a bolt icon.
+function addIconsToCalendar() {
   var visibleDays = $('.day');
 
   // looping through the days
@@ -20,7 +47,7 @@ $(document).ready(function() {
 
   var legend = $('#legend');
   addLegend(legend);
-})
+}
 
 function addBattery(day, taCount, tasRequired) {
   var icon = $('<div class="icon"></div>');
@@ -60,7 +87,7 @@ function addLegend(legendDiv) {
   medium.prepend('<i class="fa fa-bolt"></i>');
 
   var full = $('<div class="full right col-xs-4"></div>');
-  full.text(" = Full!");
+  full.text(" = Enough TAs!");
   full.prepend('<i class="icon fa fa-battery-full"></i>');
 
   legendDiv.append(empty);
